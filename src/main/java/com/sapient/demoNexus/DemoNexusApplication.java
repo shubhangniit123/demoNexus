4a984caa9a07b3f3c418d0d9ef7c9dda77db86e4
shugupta27 <shugupta27@publicisgroupe.net>
2023-06-15 16:08:37 +0530

    demo nexus

---
+++ src/main/java/com/sapient/demoNexus/DemoNexusApplication.java
@@ -0,0 +1,13 @@
+package com.sapient.demoNexus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoNexusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoNexusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sapient/demoNexus/DemoNexusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sapient.demoNexus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoNexusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
